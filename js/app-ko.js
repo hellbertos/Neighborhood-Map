ff40eb055ad7d77219dc3d005b1da7fdaf75d051
James Ruggieri <james.ruggieri@yahoo.com>
2015-12-21 19:52:38 -0800

    Add searchIt function to filter input value. Cannot get list to filter.

--- js/app-ko.js
+++ js/app-ko.js
@@ -2,6 +2,23 @@
  ** Fogbelt Neighborhood Map App with Knockout - Udacity FEND Lesson 5
 **/
 
+/**
+	ADD CUSTOM BINDING HANDLER -- Example
+
+	ko.bindingHandlers.showOrNo = {
+    init: function(element, valueAccessor) {
+        // Start visible/invisible according to initial value
+        var shouldDisplay = valueAccessor();
+        $(element).toggle(shouldDisplay);
+    },
+    update: function(element, valueAccessor) {
+        // On update, fade in/out
+        var shouldDisplay = valueAccessor();
+        shouldDisplay ? $(element).fadeIn() : $(element).fadeOut();
+    }
+};
+/************************************************/
+
 
 window.onload = function() {
 	// Set Up Data Model: The places to show on the map
@@ -108,6 +125,9 @@ window.onload = function() {
 
 		modelCxt.placeList = ko.observableArray();
 
+		// Set up in failed attempt to bind css class to a boolean
+		modelCxt.activeClass = ko.observable('true');
+
 		// Initialize Google Map with correct coordinates and options
 		var mapCanvas = document.getElementById('map');
 		// Add Latitude and Longitude options and zoom level
@@ -120,8 +140,7 @@ window.onload = function() {
 		modelCxt.map = new google.maps.Map(mapCanvas, mapOptions);
 
 		// Create global infoWindow object which will be reused for each location as per Google Maps Docs
-		modelCxt.infoWindow = new google.maps.InfoWindow();
-
+		modelCxt.infoWindow = new google.maps.InfoWindow({maxWidth: 260});
 
 		//Create the markers
 		for(var i = 0; i < localCoords.length; i++ ) {
@@ -129,7 +148,7 @@ window.onload = function() {
 
 			// create the new markers
 			var marker = (new Marker(locale.latLng, locale.title, locale.icon, locale.description, modelCxt.map) );
-
+			marker.show = ko.observable(true);
 			// Add the markers to an observable array
 			modelCxt.placeList.push(marker);
 
@@ -148,7 +167,7 @@ window.onload = function() {
 		//console.info("OBsAry item is "+modelCtx.placeList()[0].title);
 
 
-		//TEST Method
+		//Show the marker as selected
 		modelCxt.selectMarker = function(marker) {
 				//console.log("Animate: "+marker.animation+"\n"+marker.title);
 
@@ -158,7 +177,7 @@ window.onload = function() {
 				// Stop bounce
 				setTimeout(function () {
 			          marker.setAnimation(null);
-			      }, 2120);
+			      }, 2150);
 				//console.info('THEE DESCRIPTION: '+marker.description);
 
 				// Get marker description and open info window
@@ -170,6 +189,81 @@ window.onload = function() {
 
 			}
 
+		// Filter input for available markers
+
+		// Set up observable for input field and watch for change
+		modelCxt.searchString = ko.observable();
+
+		modelCxt.searchIt = function(marker) {
+			var query = modelCxt.searchString().toLowerCase();
+
+			//console.info('JUST TESTING THE TITLE:'+marker.title+' \n');
+
+			var theTest = function(list) {
+
+			    testTitle = list.title.toLowerCase();
+			    if( testTitle.indexOf(query) >= 0 ) {
+			    	list.setVisible(true);
+			    	//modelCxt.isVisible(true);
+			    	list.show= true;
+			    	} else {
+			    	list.setVisible(false);
+			    	list.show= false;
+			    	//console.info("Show is: "+list.show+" for "+list.title);
+			    	//modelCxt.isVisible(false);
+			    	//modelCxt.isActive(list, false);
+			    	}
+			    	console.log(list);
+			    //console.log("The index of "+testTitle.indexOf(query)+" is "+testTitle);
+			    //return
+			}
+
+			//this.yep = modelCxt.placeList.filter(this.theTest);
+			//console.info(modelCxt.placeList()[4].title);
+			modelCxt.placeList().filter(theTest);
+
+		}
+
+		// Set up visible class
+
+		modelCxt.isActive = function(data, test) {
+			if(test === true) {
+				//return true;
+				modelCxt.activeClass(true);
+				console.info("ACTIVE!! "+modelCxt.activeClass() );
+			} else {
+				//return false;
+				modelCxt.activeClass(false);
+				console.info("NOT!! "+modelCxt.activeClass() );
+			}
+
+
+		}
+
+		/**
+		modelCxt.isVisible = function(test){
+			//console.log("This is the "+obj.title+"\n and I am "+test);
+
+			if(test === true) {
+				//modelCxt.activeClass('visible');
+				console.info("Active CLASS: "+modelCxt.activeClass() );
+				//modelCxt.activeClass('true');
+			} else {
+				//modelCxt.activeClass('notVisible');
+				//modelCxt.activeClass('false');
+			}
+		}**/
+
+		/**modelCxt.showOrNo = function(elem) {
+			if (elem === true ) {
+				console.log("I AM TRUE "+elem);
+				return true;
+			} else {
+				console.info("I AM FALSE "+elem);
+				return false;
+			}
+		}
+		**/
 
 
 	} // END VIEW MODEL
