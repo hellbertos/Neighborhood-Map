a8142d7ad5ab45b1e271056d3b011885140608f0
James R <james.ruggieri@yahoo.com>
2015-12-26 17:06:38 -0800

    Add all the minified production files

---
+++ production/js/mapError.js
@@ -0,0 +1,1 @@
+function errorLoadingMap(){console.log("IM ERRROROROING");document.getElementById("map");map.innerHTML='<div class="error"><p>Unfortunately we were not able to load the map. Please try again later.</p><img src="img/logo-3.jpg"></div>'}
